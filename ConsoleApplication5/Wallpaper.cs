728fb50a443a588f24a00eaea694bdcd39df0ba6
asuser1 <austinswearman@msn.com>
2018-06-02 00:50:42 -0400

    Initial commit

---
+++ ConsoleApplication5/Wallpaper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+
+class Wallpaper
+{
+    Wallpaper() { }
+
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(new ThreadStart(newThread));
+        t.SetApartmentState(ApartmentState.STA);
+        t.Start();
+
+        Console.Read();
+    }
+
+    private static void newThread()
+    {
+        String path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\animate.mp4";
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("File does not exist at path " + path);
+            Console.Read();
+            return;
+        }
+
+        Form f = new Form
+        {
+            Size = new System.Drawing.Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height),
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None,
+        };
+
+        MediaElement me = new MediaElement()
+        {
+            Name = "mediaElement1",
+            Height = f.Height,
+            Width = f.Width,
+            Source = new Uri(path),
+            Stretch = System.Windows.Media.Stretch.UniformToFill,
+            LoadedBehavior = MediaState.Manual,
+        };
+
+        me.MediaEnded += new System.Windows.RoutedEventHandler(mediaEnded);
+
+        ElementHost host = new ElementHost();
+        host.Dock = DockStyle.Fill;
+        host.Child = me;
+        me.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+        f.Controls.Add(host);
+        me.Play();
+
+        Win32.setOwnership(f);
+
+        Application.EnableVisualStyles();
+        Application.Run(f);
+    }
+
+    private static void mediaEnded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        ((MediaElement)sender).Position = TimeSpan.Zero;
+        ((MediaElement)sender).Play();
+    }
+}
---
+++ ConsoleApplication5/Win32.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+class Win32
+{
+    public enum SendMessageTimeoutFlags
+    {
+        SMTO_NORMAL = 0x0,
+        SMTO_BLOCK = 0x1,
+        SMTO_ABORTIFHUNG = 0x2,
+        SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
+        SMTO_ERRORONEXIT = 0x20,
+    }
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(
+        string lpClassName,
+        string lpWindowName);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr FindWindowEx(
+        IntPtr parentHandle,
+        IntPtr childAfter,
+        string lclassName,
+        string windowTitle);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessageTimeout(
+        IntPtr hWnd,
+        uint Msg,
+        UIntPtr wParam,
+        IntPtr lParam,
+        SendMessageTimeoutFlags fuFlags,
+        uint uTimeout,
+        out UIntPtr lpdwResult);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern bool EnumWindows(
+        EnumWindowsProc lpEnumFunc,
+        IntPtr lParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+    public static void setOwnership(Form f)
+    {
+        IntPtr progman = FindWindow("Progman", null);
+        if (progman == IntPtr.Zero)
+        {
+            Console.Write("Could not find handle to Progman window.");
+            return;
+        }
+
+        UIntPtr result = UIntPtr.Zero;
+        IntPtr msgResult = SendMessageTimeout(
+            progman,
+            0x052C,
+            new UIntPtr(0),
+            IntPtr.Zero,
+            SendMessageTimeoutFlags.SMTO_NORMAL,
+            1000,
+            out result);
+        if (msgResult == IntPtr.Zero)
+        {
+            Console.Write("Failed to send message to Progman.");
+            return;
+        }
+
+        IntPtr workerw = IntPtr.Zero;
+        EnumWindows(new EnumWindowsProc((tophandle, topparamhandle) =>
+        {
+            IntPtr p = FindWindowEx(tophandle,
+                                        IntPtr.Zero,
+                                        "SHELLDLL_DefView",
+                                        "");
+
+            if (p != IntPtr.Zero)
+            {
+                workerw = FindWindowEx(IntPtr.Zero,
+                                           tophandle,
+                                           "WorkerW",
+                                           "");
+            }
+
+            return true;
+        }), IntPtr.Zero);
+
+        if (workerw == IntPtr.Zero)
+        {
+            Console.Write("Could not find handle to WorkerW window.");
+            return;
+        }
+
+        SetParent(f.Handle, workerw);
+    }
+}
